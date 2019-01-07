257da7256907f07ce651ce08d592ad8833b6d697
Liana Elisabeta <lianaelisabeta.ln@gmail.com>
2019-01-07 07:09:45 +0200

    Mici modificari

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,45 +15,18 @@ public class Main {
 
     public static void main(String[] args) {
         AnalizorLexical analizor = new AnalizorLexical();
-        analizor.getAtomi("cerc.txt");
+        analizor.getAtomi("suma.txt");
         analizor.printAtoms("fip.txt","ts.txt");
 
         FileUtils fileUtils = new FileUtils("gramatica.txt");
-//        System.out.println("Productii:");
+
         List<Productie> productii = fileUtils.getProductii();
-//        for (Productie productie : productii){
-//            System.out.println(productie.toString());
-//        }
-//        System.out.println();
-//        System.out.println("Terminali:");
         List<String> terminali = fileUtils.getTerminali();
-//        for (String terminal : terminali){
-//            System.out.println(terminal);
-//        }
-//
-//        System.out.println();
-//        System.out.println("Neterminali:");
         List<String> neterminali = fileUtils.getNeterminali();
-//        for (String neterminal : neterminali){
-//            System.out.println(neterminal);
-//        }
-//
 
         Gramatica gramatica = new Gramatica(productii,"S'",terminali,neterminali);
 
         SLR slr = new SLR(gramatica);
-        /*List<String> firsts = slr.getFirst("EXPRESIE");
-
-        for(String first : firsts){
-            System.out.println(first);
-        }*/
-
-//        List<String> follows = slr.getFollow("S");
-//
-//        for(String follow : follows){
-//            System.out.println(follow);
-//        }
-//        slr.getClosureTest();
         slr.buildStari();
         System.out.println("Conflicts:"+slr.checkIfConflicts());
         DecodificareFIP decodificareFIP = new DecodificareFIP("tabelAtomi.txt");
@@ -66,22 +39,6 @@ public class Main {
         catch (Exception e){
             System.err.println(e.getMessage());
         }
-
-
-//        List<String> secv = new ArrayList<>();
-//        secv.add("b");
-//        secv.add("i");
-//        secv.add("c");
-//       // secv.add("i");
-//        secv.add("e");
-//        try{
-//        Stack<Integer> sirproductii = slr.parseSecv(secv);
-//        while(!sirproductii.empty()){
-//            System.out.print(sirproductii.pop()+" ");
-//        }}
-//        catch (Exception e){
-//            System.err.println(e.getMessage());
-//        }
 
     }
 }
